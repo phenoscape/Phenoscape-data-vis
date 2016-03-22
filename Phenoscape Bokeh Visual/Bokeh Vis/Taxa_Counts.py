a467c6e91e8036f1d7cfd8c5f8db4352e1f704c2
Lucytheanimefan <spothorse9.lucy@gmail.com>
2016-03-22 09:53:08 -0400

    Wrote get_taxa_count method
    Method gets annotated taxa count. Parameter is the name of taxa. Uses
    url function to find json.

--- Phenoscape Bokeh Visual/Bokeh Vis/Taxa_Counts.py
+++ Phenoscape Bokeh Visual/Bokeh Vis/Taxa_Counts.py
@@ -5,21 +5,19 @@ Created on Mar 11, 2016
 '''
 import openpyxl
 import json
-from urllib2 import urlopen, Request
-from collections import OrderedDict
-
+from urllib2 import urlopen
 
 # Open spreadsheet of VTO and Term IRIs
 data=openpyxl.load_workbook('VTO.xlsx')
 sheet=data.get_sheet_by_name('Worksheet')
 print sheet['A1'].value
-
+'''
 # array of total annotated taxa counts 
 labels=[]
 num=[]
 count={}
 for i in range(2,10):
-    print(sheet.cell(row=i, column=1).value)
+    #print(sheet.cell(row=i, column=1).value)
     cellLabel=sheet.cell(row=i, column=2).value
     cellValue=sheet.cell(row=i, column=1).value
     url='http://kb.phenoscape.org/api/taxon/annotated_taxa_count?in_taxon='+cellValue
@@ -30,16 +28,31 @@ for i in range(2,10):
 count['label']=labels
 count['taxaCount']=num
     
-print count
-
+#print count
+'''
 # method finds url depending on name of taxa/term
 def get_url(nameOfTaxa):
-    for i in range(2,sheet.get_highest_row()-1):
+    for i in range(2,sheet.max_row-1):
         cellLabel=sheet.cell(row=i, column=2).value
         if cellLabel==nameOfTaxa:
             cellValue=sheet.cell(row=i, column=1).value
             print cellValue
             return cellValue
-        
+    for i in range(4,sheet.max_row-1):
+        cellLabel=sheet.cell(row=i, column=4).value
+        if cellLabel==nameOfTaxa:
+            cellValue=sheet.cell(row=i, column=3).value
+            print cellValue
+            return cellValue
+
+# method gets annotated taxa count
+# parameter is the name of taxa         
+def get_taxa_count(nameofTaxa):
+    value=get_url(nameofTaxa)
+    url='http://kb.phenoscape.org/api/taxon/annotated_taxa_count?in_taxon='+value
+    taxaCount=json.loads(urlopen(url).read())
+    return taxaCount['total']
+    
 get_url('Hominoidea')
+print get_taxa_count('Siluriformes')
 
