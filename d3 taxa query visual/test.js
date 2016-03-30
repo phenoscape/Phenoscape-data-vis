24d574fdf0f7711b294ae1b29bee9255bf2101be
Lucytheanimefan <spothorse9.lucy@gmail.com>
2016-03-29 22:57:29 -0400

    Updated graph to get descendants when bar is clicked

--- d3 taxa query visual/test.js
+++ d3 taxa query visual/test.js
@@ -1,17 +1,23 @@
+var uberon;
+
 function getName(purlURL){
-  var url='http://kb.phenoscape.org/api/term?iri='+purlURL
+  var url='http://kb.phenoscape.org/api/term?iri=http://purl.obolibrary.org/obo/UBERON_'+purlURL
   $.getJSON(url, function(json) {
         console.log(json.label);
         return json.label;
       });
 }
 
-function getUberon(purlURL){
-  console.log(purlUrl.substr(purlURL.length-7));
+function getUberon(purlUrl){
+  console.log("Uberon: " +purlUrl.substr(purlUrl.length-7));
+  uberon=purlUrl.substr(purlUrl.length-7);
+  return uberon;
 }
 
 //console.log("Here:"+getName("http://purl.obolibrary.org/obo/UBERON_0011618"));
 //getUberon("http://purl.obolibrary.org/obo/UBERON_0011618");
 
-var name=getName("http://purl.obolibrary.org/obo/UBERON_0011618");
+var name=getName("0011618");
 console.log("Name: "+name);
+getUberon("http://purl.obolibrary.org/obo/UBERON_0011618");
+console.log(getUberon("http://purl.obolibrary.org/obo/UBERON_0011618"));
